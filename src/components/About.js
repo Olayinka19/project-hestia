31c45a6a611163b98606a05f54dd826fabaf3aa4
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2022-05-22 00:49:28 -0400

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
-      <h1>We litt</h1>
-      <h1>pull it</h1>
-      <p>adding it</p>
-    </div>
+    <div>About</div>
     
   )
 }
