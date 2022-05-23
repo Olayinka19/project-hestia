744547cb7eb52fa341547c19ef5c90757284108b
John Prado <jcprado20@icloud.com>
2022-05-22 21:35:36 -0400

    add team

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <div>Team
+      <ul>
+        <li>
+          Adnan
+        </li>
+        <li>
+          Mason
+        </li>
+        <li>
+          John
+        </li>
+        <li>
+          Olayinka
+        </li>
+      </ul>
+    </div>
+    
   )
 }
 
